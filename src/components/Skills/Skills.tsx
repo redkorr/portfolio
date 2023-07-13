d95e8e11b4e825f73e5c9b3848b1d55f87f41b73
redkorr <redkor910.97@gmail.com>
2023-07-13 13:05:11 +0200

    feat: created Skills component

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Skills = () => {
+  const Skills = ['Quick learning', 'Teamwork', 'Native polish', 'B2 english', 'Time-management', 'Organization'];
+
+  return (
+    <div>
+      <h2 className="text-4xl font-semibold py-11">Skills</h2>
+      <div className="flex flex-wrap">
+        {Skills.map((skill) => (
+          <div
+            key={skill}
+            className="flex items-center w-1/3 text-2xl p-auto"
+          >
+            <div className="p-[10px] h-[10px] mr-4 rounded-2xl bg-gradient-to-r from-darkBlue to-neonBlue"></div>
+            {skill}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
