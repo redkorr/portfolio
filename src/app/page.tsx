112fc5da4b271b702dae57c2ee8d57bd77aee92d
redkorr <redkor910.97@gmail.com>
2023-07-27 13:49:18 +0200

    checkpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,20 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function Home() {
   return (
-    <main className="mx-auto w-3/5 scroll-smooth">
-      <ParallaxProvider>
-        <BackgroundAnimation />
-      </ParallaxProvider>
-      <Header />
-      <HeroSection />
-      <ProjectsSection />
-      <Technologies />
-      <Skills />
-      <Footer />
+    <main className="mx-auto w-3/5 scroll-smooth relative z-20">
+      <div className="background">
+        <ParallaxProvider>
+          <BackgroundAnimation />
+        </ParallaxProvider>
+      </div>
+      <div className="foreground">
+        <Header />
+        <HeroSection />
+        <ProjectsSection />
+        <Technologies />
+        <Skills />
+        <Footer />
+      </div>
     </main>
   );
 }
--- src/components/BackgroundAnimation/BackgroundAnimation.tsx
+++ src/components/BackgroundAnimation/BackgroundAnimation.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { useParallax } from 'react-scroll-parallax';
+
 const BackgroundAnimation = () => {
-  return <div></div>;
+  const { ref } = useParallax<HTMLDivElement>({ speed: -10 });
+  return (
+    <div
+      ref={ref}
+      className="fixed inset-x-1/2 inset-y-1/4 z-10"
+    >
+      <div className="w-20 h-20 rounded-full bg-gradient-to-r from-darkBlue to-neonBlue"></div>
+      <div className="w-52 h-52 rounded-full bg-transparent border-neonBlue border"></div>
+      <div className="w-80 h-80 rounded-full bg-transparent border-neonBlue border"></div>
+    </div>
+  );
 };
 
 export default BackgroundAnimation;
