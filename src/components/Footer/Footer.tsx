9981a6df811dae3942de5dc5058f074ed01a37a9
redkorr <redkor910.97@gmail.com>
2023-09-01 18:00:31 +0200

    fix: changed path to contact form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,11 +6,11 @@ import Links from '../Links/Links';
 
 const Footer = () => {
   return (
-    <div className="flex justify-between items-center py-10">
+    <div className="flex justify-between items-center lg:pt-20 pt-10">
       <div className="text-transparent font-semibold bg-clip-text bg-gradient-to-r from-darkBlue to-neonBlue">
-        <Link href="http://localhost:3000/contact">Feel free to cantact me</Link>
+        <Link href="/contact">Feel free to cantact me</Link>
       </div>
-      <div>
+      <div className="lg:inline hidden">
         <a>a.kemicer.contact@gmail.com</a>
       </div>
       <div>
